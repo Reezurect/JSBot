62d13199f2bfec8919e691e6f163d22a1eafa804
Denis Zudilin <zudilin2014@yandex.ru>
2023-10-12 15:51:52 +0300

    HardBot

--- script.js
+++ script.js
@@ -10,33 +10,46 @@ const isNum = n => {
 
 const start = () => {
     let rNumber = getRandomInt(100);
+    console.log('rNumber: ', rNumber);
+    let nAttempts = 10;
     const game = () => {
-        const num = prompt('"Угадай число от 1 до 100" (Для выхода оставьте строку пустой)');
-        if (num === null) {
-            alert('До свидания');
-            return;
-        }
-        if (isNum(num)) {
-            const realNum = +num;
-            if (realNum > rNumber) {
-                alert('Загаданное число меньше');
-                game();
-            } else if (realNum < rNumber) {
-                alert('Загаданное число больше');
-                game();
+        nAttempts--;
+        if (nAttempts < 0) {
+            if (confirm('Попытки закончились, хотите сыграть еще?')) {
+                start();
             } else {
-                if (confirm('Вы угадали! Сыграем ещё?')) {
-                    start();
-                } else {
-                    alert('До свидания');
-                    return;
-                }
-            }
+                alert('До свидания');
+                return;
+            };
         } else {
-            alert('Введите число');
-            game();
-        }
+            const num = prompt('"Угадай число от 1 до 100" (Для выхода оставьте строку пустой)');
+            if (num === null) {
+                alert('До свидания');
+                return;
+            };
+            if (isNum(num)) {
+                const realNum = +num;
+                if (realNum > rNumber) {
+                    alert(`Загаданное число меньше, осталось попыток: ${nAttempts}`);
+                    game();
+                } else if (realNum < rNumber) {
+                    alert(`Загаданное число больше, осталось попыток: ${nAttempts}`);
+                    game();
+                } else {
+                    if (confirm('Поздравляю, Вы угадали!!! Хотели бы сыграть еще?')) {
+                        start();
+                    } else {
+                        alert('До свидания');
+                        return;
+                    };
+                };
+            } else {
+                alert('Введите число');
+                game();
+            };
+        };
     };
+    console.dir(game);
     game();
 };
 
